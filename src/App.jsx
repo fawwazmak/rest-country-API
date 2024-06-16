e3feabd25bfac55c463d161a1559b13d6c2e9b05
fawwazmak <fawwazmakinde47@gmail.com>
2024-06-16 17:30:52 +0100

    notified working on project

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
-  
   // https://restcountries.com
 
   return (
     <>
+      <p className="position-absolute bg-info fw-bolder fs-1 end-50 bottom-0">
+        Still working on this project!!
+      </p>
       <Homepage />
     </>
   );
