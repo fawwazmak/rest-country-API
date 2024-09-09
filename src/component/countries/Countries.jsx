54b6d76fc1740882c08bb72bad34b6af1a5e225c
unknown <you@example.com>
2024-09-09 18:03:09 +0100

    having issue with row gap

--- src/component/countries/Countries.jsx
+++ src/component/countries/Countries.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import '../../index.css';
 import Button from "react-bootstrap/esm/Button";
 
 const Countries = ({ theme, countriesArray, searchedCountry, countryRegion }) => {
@@ -6,32 +7,29 @@ const Countries = ({ theme, countriesArray, searchedCountry, countryRegion }) =>
     <>
       <div className="mt-5 mb-3">
         <div className="container">
-          <div className="row">
+          <div className="row row-gap-5">
             {countriesArray
               .filter((country) => country.name.common.toLowerCase().includes(searchedCountry.toLowerCase())).filter((country) => country.region.toLowerCase().includes(countryRegion.toLowerCase()))
               .map((country, index) => (
-                <div
-                  key={index}
-                  className={`col-12 ge-5 col-md-6 col-lg-3 rounded-3 ${theme === "light" ? "bg-on-light" : "bg-on-dark"}`}
-                >
-                  <img
-                    src={country.flags.svg}
-                    alt={`${country.name.common} Flag`}
-                    className="w-100"
-                  />
-                  <div className="p-3">
-                    <h4>{country.name.common}</h4>
-                    <p>
-                      <span className="fw-bold">Population:</span>{" "}
-                      {country.population}
-                    </p>
-                    <p>
-                      <span className="fw-bold">Region:</span> {country.region}
-                    </p>
-                    <p>
-                      <span className="fw-bold">Capital:</span>{" "}
-                      {country.capital}
-                    </p>
+                <div className="col-12 col-md-6 col-lg-3 rounded-2 p-0">
+                  <div key={index} className={`h-100 p-0 ${theme === "light" ? "bg-on-light" : "bg-on-dark"}`}>
+                    <img src={country.flags.svg} alt={`${country.name.common} Flag`} className="w-100 h-50" />
+                    <div className="p-3">
+                      <h4>{country.name.common}</h4>
+                      <p>
+                        <span className="fw-bold">Population:</span>{" "}
+                        {country.population}
+                      </p>
+
+                      <p>
+                        <span className="fw-bold">Region:</span> {country.region}
+                      </p>
+                      
+                      <p>
+                        <span className="fw-bold">Capital:</span>{" "}
+                        {country.capital}
+                      </p>
+                    </div>
                   </div>
                 </div>
               ))}
